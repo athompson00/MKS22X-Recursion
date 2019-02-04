6f766a2ac69f1c6f3f9d8c0170a05e858a0d23d0
athompson00 <athompson00@stuy.edu>
2019-02-03 20:37:41 -0500

    created makeAllSums

--- recursion.java
+++ recursion.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class recursion{
   public static double pError(double guess, double actual){
     return Math.abs(guess - actual) / actual * 100.0;
@@ -26,10 +27,28 @@ public class recursion{
     }
   }
 
+  public static ArrayList<Integer> makeAllSums(int n){
+    ArrayList<Integer> result = new ArrayList<Integer>();
+    return mAS(n, 0, result);
+  }
+
+  public static ArrayList<Integer> mAS(int n, int sum, ArrayList<Integer> a){
+    if (n == 1){
+      a.add(sum);
+    } else {
+      mAS(n - 1, sum + n, a);
+      mAS(n - 1, sum, a);
+    }
+    return a;
+  }
+
   public static void main(String[] args){
     System.out.println("Square root of 100: " + sqrt(100.0));
     System.out.println("Square root of 64: " + sqrt(64.0));
     System.out.println("30th term of fib sequence: " + fib(30));
     System.out.println("20th term of fib sequence: " + fib(20));
+    System.out.println("/n");
+    System.out.println("makeAllSums(5): " + makeAllSums(5));
+    System.out.println("makeAllSums(8): " + makeAllSums(8));
   }
 }
